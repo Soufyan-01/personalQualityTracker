7959fa77357046008e6d6a03a143f0d50d33e2f9
Chahi <soufyan.a.chahi@capgemini.com>
2023-03-03 10:42:42 +0100

    Initial commit

---
+++ src/test/java/com/example/personalQualityTracker/PersonalQualityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalQualityTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalQualityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
